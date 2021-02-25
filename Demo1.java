35593e23c565548f0751066d71d606ea82996ade
Sudharshan-IT <sudharshanpingme@gmail.com>
2021-02-25 11:06:21 +0530

    added New Variables

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,7 @@
 public class Demo{
       
        public String hello(){
+         String msg="helllo Good Morning..."
              return "Sudharshan";
 }
 }   
---
+++ WishMessaage.java
@@ -0,0 +1,7 @@
+public class WishMessage{
+  
+    public String wish(Stirng name){
+       Stirng msg="Good Morning..."
+      return msg;
+    }
+}
